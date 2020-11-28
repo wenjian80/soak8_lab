e424e1df11b6eda507cbfbe7ff00734fba536cb8
wenjian80 <wenjian80@gmail.com>
2020-11-28 15:56:38 +0800

    commit

--- scripts/12_Mount_File.sh
+++ scripts/12_Mount_File.sh
@@ -5,7 +5,7 @@ sudo mkdir -p /soashared
 
 #PUT FILE SYSTEM IP HERE
 #Eg sudo mount 10.0.0.5:/soashared /soashared
-sudo mount [INPUT_ID]:/soashared /soashared
+sudo mount IPCHANGE:/soashared /soashared
 
 df -h /soashared
 
