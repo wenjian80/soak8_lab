3833291816afdb4f2c8613bac586f17965ad432f
wenjian80 <wenjian80@gmail.com>
2020-11-28 14:40:37 +0800

    Update 7_Kube_proxy.sh

--- scripts/7_Kube_proxy.sh
+++ scripts/7_Kube_proxy.sh
@@ -1,5 +1,7 @@
 # Kube Porxy dashbaord
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
 kubectl create serviceaccount dashboard -n default
 
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
