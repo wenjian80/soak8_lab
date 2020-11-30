99be3c8221d84840106657626cda8b8ad34dfeb7
wenjian80 <33510286+wenjian80@users.noreply.github.com>
2020-11-30 17:03:18 +0800

    commit

--- scripts/7_Kube_proxy.sh
+++ scripts/7_Kube_proxy.sh
@@ -1,9 +1,10 @@
 # Kube Porxy dashbaord
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+kubectl apply -f dashboard-admin.yaml
 
-kubectl create serviceaccount dashboard -n default
+kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
 
-kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
-kubectl get secret $(kubectl get serviceaccount dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
 
+#Run kubectl proxy --address='0.0.0.0' --port=8001 --accept-hosts='.*'
+#Access k8 dashboard http://[masterip]:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
