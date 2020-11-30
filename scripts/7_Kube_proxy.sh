746bbbe03d933f98ed64fc1752bf2eb42dd0591b
wenjian80 <wenjian80@gmail.com>
2020-11-30 17:58:04 +0800

    Update 7_Kube_proxy.sh

--- scripts/7_Kube_proxy.sh
+++ scripts/7_Kube_proxy.sh
@@ -3,8 +3,9 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-admin.yaml
 
-kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
+echo "Token to login to dashboard"
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
 
 
-#Run kubectl proxy --address='0.0.0.0' --port=8001 --accept-hosts='.*'
-#Access k8 dashboard http://[masterip]:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
+#Run kubectl proxy
+#Access k8 dashboard http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
