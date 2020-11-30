fcab72eca0ef05f1c3360d3d962f7cf9abdb52d6
wenjian80 <33510286+wenjian80@users.noreply.github.com>
2020-11-30 09:25:08 +0800

    Update 16_Traefik_LB.sh

--- scripts/16_Traefik_LB.sh
+++ scripts/16_Traefik_LB.sh
@@ -1,14 +1,14 @@
 kubectl create namespace traefik
 
-helm repo add stable https://charts.helm.sh/stable
-
 cd /home/opc/weblogic-kubernetes-operator
-helm install traefik-operator  stable/traefik  --namespace traefik --values kubernetes/samples/charts/traefik/values.yaml --set  "kubernetes.namespaces={traefik}" --wait
+helm repo add traefik https://containous.github.io/traefik-helm-chart
+ 
+helm install traefik  traefik/traefik --namespace traefik --values kubernetes/samples/scripts/charts/traefik/values.yaml --set  "kubernetes.namespaces={traefik}" --set "service.type=NodePort" --wait
 
-kubectl get svc -n traefik
+kubectl get all -n traefik
  
 #Configure Traefik to manage Ingresses created in this namespace:
-helm upgrade --reuse-values --set "kubernetes.namespaces={traefik,soans}" --wait traefik-operator stable/traefik --namespace traefik
+helm upgrade traefik traefik/traefik --namespace traefik  --reuse-values --set "kubernetes.namespaces={traefik,soans}"
 
 #Generate sample cert for ssl 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls1.key -out /tmp/tls1.crt -subj "/CN=*"
