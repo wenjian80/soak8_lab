5c140881a43c5543c8022b409bb0a6001387c328
wenjian80 <wenjian80@gmail.com>
2020-12-03 10:13:33 +0800

    Update 16_Traefik_LB.sh

--- scripts/16_Traefik_LB.sh
+++ scripts/16_Traefik_LB.sh
@@ -2,6 +2,9 @@ kubectl create namespace traefik
 helm repo add traefik https://containous.github.io/traefik-helm-chart
 
 
+#cd /home/opc/weblogic-kubernetes-operator
+#helm install traefik  traefik/traefik --namespace traefik --values kubernetes/samples/scripts/charts/traefik/values.yaml --set  "kubernetes.namespaces={traefik}" --set "service.type=NodePort" --wait
+
 cd /home/opc/fmw-kubernetes/OracleSOASuite/
 helm install traefik  traefik/traefik --namespace traefik --values kubernetes/charts/traefik/values.yaml --set  "kubernetes.namespaces={traefik}" --set "service.type=NodePort" --wait
  
@@ -14,6 +17,12 @@ helm upgrade traefik traefik/traefik --namespace traefik  --reuse-values --set "
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/tls1.key -out /tmp/tls1.crt -subj "/CN=*"
 kubectl -n soans create secret tls soainfra-tls-cert --key /tmp/tls1.key --cert /tmp/tls1.crt
 
+
+
+
+#cd /home/opc/weblogic-kubernetes-operator	 
+#helm install soa-traefik-ingress  kubernetes/samples/charts/ingress-per-domain --namespace soans --values /home/opc/soak8_lab/scripts/ingress_domainvalues.yaml 
+	 
 cd /home/opc/fmw-kubernetes/OracleSOASuite/
 helm install soainfra-ingress  kubernetes/ingress-per-domain  --namespace soans --values /home/opc/soak8_lab/scripts/ingress_domainvalues.yaml 
 
