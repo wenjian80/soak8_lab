18942cdf1c5ae1685c703b2e08abcc4d52c8c99c
wenjian80 <wenjian80@gmail.com>
2020-12-03 10:01:17 +0800

    Update 9_Operator.sh

--- scripts/9_Operator.sh
+++ scripts/9_Operator.sh
@@ -23,3 +23,26 @@ kubectl get pods -n opns
 kubectl get all -n opns
 
 #kubectl logs -n opns -c weblogic-operator deployments/weblogic-operator
+
+
+#TO TEST
+#TO CHANGE ALL PATH IN REST OF THE SCRIPTS
+
+#export WORKDIR=/home/opc/
+
+
+#mv -f ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/create-soa-domain  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/create-soa-domain_backup
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/create-soa-domain  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/
+
+#mv -f ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/common  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/common_backup
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/common  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/
+
+#mv -f ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/create-rcu-schema  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/create-rcu-schema_backup
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/create-rcu-schema  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/
+
+#mv -f ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/charts/ingress-per-domain  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/charts/ingress-per-domain_backup
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/ingress-per-domain  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/charts/
+
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/imagetool-scripts  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/
+
+#cp -rf ${WORKDIR}/fmw-kubernetes/OracleSOASuite/kubernetes/charts  ${WORKDIR}/weblogic-kubernetes-operator/kubernetes/samples/scripts/
